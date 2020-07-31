6121a494066a9d73c835b43c86ce769f6f135348
Courtney Senior <courtney@flatfair.co.uk>
2020-07-31 10:26:27 +0100

    latest keymap for HHKB Tofu

--- keyboards/kbdfans/kbd6x/keymaps/ceedark/keymap.c
+++ keyboards/kbdfans/kbd6x/keymaps/ceedark/keymap.c
@@ -20,7 +20,7 @@
 
 #define TAPPING_TOGGLE = 2
 
-#define CTL_CAPS LT(_FN_LAYER, KC_CAPSLOCK)
+#define CTL_CAPS MT(MOD_LCTL, KC_CAPSLOCK)
 #define TAB_FN LT(_FN_LAYER, KC_TAB)
 
 
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TAB_FN  , KC_Q,    KC_W   , KC_E,    KC_R,    KC_T,     KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
       CTL_CAPS, KC_A,    KC_S   , KC_D,    KC_F,    KC_G,     KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT , KC_Z,    KC_X   , KC_C,    KC_V,    KC_B,     KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, TT(_FN_LAYER),
-                    KC_NO, KC_LALT, KC_LGUI                    KC_SPACE,                 KC_RALT, KC_RGUI, KC_NO
+                    KC_NO, KC_LALT, KC_LGUI,                    KC_SPACE,                 KC_RALT, KC_RGUI, KC_NO
       ),
 
   [_FN_LAYER] = LAYOUT(
